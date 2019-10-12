34ad5f8baa42ed60447bd45c7086291f2e73c2dd
xtreamui <56234262+xtreamui@users.noreply.github.com>
2019-10-12 02:48:24 +0100

    Fix auto-update.

--- crons/update.php
+++ crons/update.php
@@ -4,9 +4,9 @@ $rPath = "/home/xtreamcodes/iptv_xtream_codes/adtools/settings.json";
 $rSettings = json_decode(file_get_contents($rPath), True);
 
 if (($rSettings) && ($rSettings["auto_update"])) {
-    if (time() - $rSettings["auto_update_check"] > $rSetings["auto_update_periodicity"]) {
+    if (time() - $rSettings["auto_update_check"] > $rSettings["auto_update_periodicity"]) {
         $rSettings["auto_update_check"] = time();
-        $rUpdate = file_get_contents($rSettings["git_url"]."/master/adtools/settings.json");
+        $rUpdate = json_decode(file_get_contents("https://raw.githubusercontent.com/".$rSettings["git_url"]."/master/adtools/settings.json"), True);
         if (($rUpdate["version"]) && (intval($rUpdate["version"]) > intval($rSettings["version"]))) {
             // New version available!
             exec($rUpdate["update_script"]);
